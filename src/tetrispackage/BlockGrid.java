993ffae0b08d5e434e01e1270c12e28d65b38405
RedoRojo <cristhiabnvictorrm@gmail.com>
2024-03-24 18:21:50 -0400

    Comentarios borrados en BlockGrid

--- src/tetrispackage/BlockGrid.java
+++ src/tetrispackage/BlockGrid.java
@@ -103,7 +103,6 @@ public class BlockGrid
 	{
 		BlockGrid grid = new BlockGrid(10,20);
 		TetrisBlock.RIGHT_L.setLocation(0,2).draw(grid);
-//		TetrisBlock.RIGHT_L.setLocation(1,15).rotateLeft().draw(grid);
 		String logMessage = grid.toString();
 		LOGGER.info(logMessage);
 	}
